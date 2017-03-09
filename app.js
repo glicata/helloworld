9b7f2960641b25ab2707db1eafacb8cbd187e487
glicata <glicata@rightanswers.com>
2017-03-09 16:18:23 -0500

    Created app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
-*/
+
 bot.dialog('/', [
     function (session) {
         session.beginDialog('/askName');
@@ -34,5 +34,40 @@ bot.dialog('/askName', [
     },
     function (session, results) {
         session.endDialogWithResult(results);
+    }
+]);*/
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/ensureProfile', session.userData.profile);
+    },
+    function (session, results) {
+        session.userData.profile = results.response;
+        session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
+    }
+]);
+bot.dialog('/ensureProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {};
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "What's your name?");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.company) {
+            builder.Prompts.text(session, "What company do you work for?");
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
     }
 ]);
