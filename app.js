410a07ace32b7cf411009b3e0a7adcd74b01e9df
glicata <glicata@rightanswers.com>
2017-03-09 15:14:03 -0500

    Created app.js and bot_test.html

--- app.js
+++ app.js
@@ -11,10 +11,28 @@ server.listen(process.env.PORT || 3000, function () {
 // Create chat bot
 var connector = new builder.ChatConnector
     ({ appId: 'd0f033b0-dc80-463e-8c72-dcd8e8b8001d', appPassword: 'RQkgmncpxUU3V3WFiwcOdT3' });
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+//var bot = new builder.UniversalBot(connector);
+//server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
+/* Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+*/
+var bot = new builder.TextBot();
+bot.add('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
+});
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
