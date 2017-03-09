1aeea0edfc78611c97f1a5a2be20abda753a8165
glicata <glicata@rightanswers.com>
2017-03-09 14:33:33 -0500

    Created package.json and app.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # helloworld
 this is a repository for a hello world app
+
+this is an addition to the readme file
---
+++ app.js
@@ -0,0 +1,20 @@
+// Add your requirements
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+    ({ appId: 'd0f033b0-dc80-463e-8c72-dcd8e8b8001d', appPassword: 'RQkgmncpxUU3V3WFiwcOdT3' });
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
