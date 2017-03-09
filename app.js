53934a50d9f9192d2d15ecfe9bbea3c1586657b8
glicata <glicata@rightanswers.com>
2017-03-09 16:02:01 -0500

    Created app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Add your requirements
+/*/ Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -18,3 +18,30 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+*/
+var builder = require('botbuilder');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
