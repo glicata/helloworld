fe5ead49e1dc9ec2dcbc11bffe9abd8a5e60e199
glicata <glicata@rightanswers.com>
2017-03-09 16:08:30 -0500

    Created app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*/ Add your requirements
+/ Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -8,18 +8,18 @@ server.listen(process.env.PORT || 3000, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-*/// Create chat bot
+// Create chat bot
 var connector = new builder.ChatConnector
     ({ appId: 'd0f033b0-dc80-463e-8c72-dcd8e8b8001d', appPassword: 'RQkgmncpxUU3V3WFiwcOdT3' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
 /*
 // Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
 */
-var builder = require('botbuilder');
 
 
 
