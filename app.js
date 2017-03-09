0ad1650468d791d7fe7844f1590d20fe246484ff
glicata <glicata@rightanswers.com>
2017-03-09 16:15:21 -0500

    Created app.js

--- app.js
+++ app.js
@@ -15,7 +15,24 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
-// Create bot dialogs
+/*/ Create bot dialogs
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+*/
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/askName');
+    },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
+bot.dialog('/askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
