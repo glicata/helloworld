d92022525bd551ac6f589cb81d51e0ae056a75d6
glicata <glicata@rightanswers.com>
2017-03-21 09:42:27 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ bot.dialog('/', function (session) {
 
 
 //
-
+/*
 bot.dialog('/', [
     function (session) {
         builder.Prompts.text(session, 'Hi! What is your name?');
@@ -34,3 +34,44 @@ bot.dialog('/', [
         session.send('Hello %s!', results.response);
     }
 ]);
+*/
+
+
+
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/ensureProfile', session.userData.profile);
+    },
+    function (session, results) {
+        session.userData.profile = results.response;
+        session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
+    }
+]);
+bot.dialog('/ensureProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {};
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "What's your name?");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.company) {
+            builder.Prompts.text(session, "What company do you work for?");
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
+    }
+]);
+
+
