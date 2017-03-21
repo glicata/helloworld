ad076bf58d17b9e977736bfd2e61eaa850dec51e
glicata <glicata@rightanswers.com>
2017-03-21 09:37:07 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -19,3 +19,16 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
+
+
+
+//
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
