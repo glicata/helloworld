e91d6810e7f855b71e7191b3200900d6a6e7a322
glicata <glicata@rightanswers.com>
2017-03-21 09:53:02 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -85,7 +85,10 @@ bot.dialog('/ensureProfile', [
 
 bot.dialog('/', new builder.IntentDialog()
     .matches(/^hello/i, function (session) {
-        session.send("Hi there!");
+        session.send("Hi there! Ask me what the weather is like today!");
+    })
+    .matches(/^What's the weather like today?/i, function (session) {
+        session.send("The weather in Edison today is overcast with a high of 50'F. The Skies will clear by this afternoon");
     })
     .onDefault(function (session) {
         session.send("I didn't understand. Say hello to me!");
