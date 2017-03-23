6d5a416bdf9d5d51a06cfebcef4a9700519d3aad
glicata <glicata@rightanswers.com>
2017-03-23 10:04:13 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ bot.dialog('/', [
     },
     function (session, results) {
         session.userData.profile = results.response;
-        session.send('Hello %(name)s! I love %(company)s!', session.userData.profile);
+        session.send('Hello %(name)s! I love %(lastname)s!', session.userData.profile);
     }
 ]);
 bot.dialog('/ensureProfile', [
@@ -63,15 +63,15 @@ bot.dialog('/ensureProfile', [
         if (results.response) {
             session.dialogData.profile.name = results.response;
         }
-        if (!session.dialogData.profile.company) {
-            builder.Prompts.text(session, "What company do you work for?");
+        if (!session.dialogData.profile.lastname) {
+            builder.Prompts.text(session, "What lastname do you work for?");
         } else {
             next();
         }
     },
     function (session, results) {
         if (results.response) {
-            session.dialogData.profile.company = results.response;
+            session.dialogData.profile.lastname = results.response;
         }
         session.endDialogWithResult({ response: session.dialogData.profile });
     }
@@ -176,32 +176,38 @@ bot.dialog('/', [
 
 
 bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/ensureProfile', session.userData.profile);
+    },
+    function (session, results) {
+        session.userData.profile = results.response;
+        session.send('Hello %(name)s! I love %(lastname)s!', session.userData.profile);
+    }
+]);
+bot.dialog('/ensureProfile', [
     function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
+        session.dialogData.profile = args || {};
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "What's your name?");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.lastname) {
+            builder.Prompts.text(session, "What lastname do you work for?");
         } else {
             next();
         }
     },
     function (session, results) {
-        session.send('Hello %(name) %(lname)!', session.userData.name);
-    }
-]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-       session.endDialog();
-    },
-    function (session) {
-        builder.Prompts.text(session, 'And whats your last name?');
-    },
-    function (session, results) {
-        session.userData.lname = results.response;
-        session.endDialog();
+        if (results.response) {
+            session.dialogData.profile.lastname = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
     }
 ]);
 
