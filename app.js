4b4d63da3fb40f878dea117885a7e5da7344cc27
glicata <glicata@rightanswers.com>
2017-03-23 10:26:51 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ bot.dialog('/', [
     },
     function (session, results) {
         session.userData.profile = results.response;
-        session.send('Hello %(name), %(lastname)!', session.userData.profile);
+        session.send('Hello First name %(name) and Last name  %(lastname)!', session.userData.profile);
     }
 ]);
 bot.dialog('/ensureProfile', [
