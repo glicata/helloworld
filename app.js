6ad9626f0baf7805e57dbcb0697807a0d98ec1f3
glicata <glicata@rightanswers.com>
2017-03-23 09:27:41 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -141,6 +141,37 @@ bot.dialog('/askLname', [
 */
 // the above sesion dialog can be wiped out by using this delete session dialog.
 
+var salesData = {
+    "west": {
+        units: 200,
+        total: "$6,000"
+    },
+    "central": {
+        units: 100,
+        total: "$3,000"
+    },
+    "east": {
+        units: 300,
+        total: "$9,000"
+    }
+};
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.choice(session, "Which region would you like sales for?", salesData);
+    },
+    function (session, results) {
+        if (results.response) {
+            var region = salesData[results.response.entity];
+            session.send("We sold %(units)d units for a total of %(total)s.", region);
+        } else {
+            session.send("ok");
+        }
+    }
+]);
+
+
+
 
 
 bot.dialog('/delete', (session) => {
